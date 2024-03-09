db2e8bf7772bc50fb4bb4e06e5e5a931075b6909
Saad Tariq <saadtariq.dev@outlook.com>
2024-03-09 02:18:44 -0500

    chore: add sitemap and icon pacakges, integrate with config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://crce.mcmaster.ca",
+  integrations: [tailwind(), icon(), sitemap()],
 });

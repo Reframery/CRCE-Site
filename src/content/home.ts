e9947d48293285ed43fa0617f24e8057d2218514
Saad Tariq <saadtariq.dev@outlook.com>
2024-03-09 02:24:23 -0500

    feat: add sample content files

---
+++ src/content/home.ts
@@ -0,0 +1,47 @@
+import focus1 from "@/images/home/focus-1.jpg";
+import focus2 from "@/images/home/focus-2.jpg";
+import focus3 from "@/images/home/focus-3.jpg";
+import vision1 from "@/images/home/vision-1.jpg";
+import vision2 from "@/images/home/vision-2.jpg";
+
+export const focusGroups = [
+  {
+    title: "Research",
+    description:
+      "At the MDTRC, we engage in cutting-edge multidisciplinary research aimed at better understanding critical implications of digital transformation. We are open to local and international collaborations with academic colleagues as well as organizations undergoing their own transformations.",
+    action: "Research With Us",
+    image: focus1,
+    href: "/cutting-edge-multidisciplinary-research",
+  },
+  {
+    title: "Outreach",
+    description:
+      "We're actively building collaborations and engaging with joint research projects with key partners and affiliates in academia and the public, private and not-for-profit sectors. Some projects are eligible for securing additional funding from various grant bodies.",
+    image: focus2,
+    action: "Learn More",
+    href: "/cutting-edge-multidisciplinary-research",
+  },
+  {
+    title: "Education",
+    description:
+      "By supporting our executive and graduate educational programs, we aim to empower managers, employees, and students, building their capabilities to effectively manage and lead digital transformation.",
+    image: focus3,
+    action: "Learn More",
+    href: "/cutting-edge-multidisciplinary-research",
+  },
+];
+
+export const visionMission = [
+  {
+    title: "Vision",
+    description:
+      "The McMaster Digital Transformation Research Centre's (MDTRC) vision is to become a self-sustaining world leading research centre and knowledge hub. We primarily focus on the leadership and management of digital transformation and its impact on people, organizations, and society.",
+    image: vision1,
+  },
+  {
+    title: "Mission",
+    description:
+      "The centre's mission is to engage in innovative research projects that investigate the booming digital innovations that are prevailing through and transforming all aspects of our modern society. This timely and important research will lead to a fundamental shift in our understanding of the developing digital revolution and its challenges and opportunities, with far-reaching implications for managerial practice and society at large. Student and executive training at the MDTRC will contribute to the development of a unique breed of Highly Qualified Personnel (HQP) with specialized skill sets, enabling them to become the digital transformation leaders of tomorrow.",
+    image: vision2,
+  },
+];

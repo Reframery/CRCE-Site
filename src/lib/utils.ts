ac93287bedb25abeaa6ffff95e631dd288ae36ca
Saad Tariq <saadtariq.dev@outlook.com>
2024-03-22 10:08:09 -0400

    refactor: extract active function to utils, use in breadcrumbs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,6 @@ import { twMerge } from "tailwind-merge";
  * Merge tailwind classes into a single string
  */
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const isActive = (href: string, pathname: string) =>
+  href === pathname || href === pathname.replace(/\/$/, "");
